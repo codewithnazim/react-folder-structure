b73e8c72b8b9e7fcac473e498e6ce312361cc290
Nazim <pungliyabhushan@gmail.com>
2022-03-29 16:41:14 +0530

    added scroll to top and 404

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter, useLocation } from "react-router-dom";
 import Index from "./ui/pages/index";
 import Blog from "./ui/pages/blog";
 import Item from "./ui/pages/blog/item";
+import Page404 from "./ui/pages/404";
 
 import { ThemeProvider } from "@mui/material/styles";
 import theme from './ui/theme/theme';
@@ -24,15 +25,18 @@ function App() {
     <div>
       <ThemeProvider theme={theme} >
         <BrowserRouter>
-          <Routes>
-            <Route path="/">
-              <Route index element={<Index />} />
-              <Route path="blog">
-                <Route index element={<Blog />} />
-                <Route path="item" element={<Item />} />
+          <Wrapper>
+            <Routes>
+              <Route path="/">
+                <Route index element={<Index />} />
+                <Route path="blog">
+                  <Route index element={<Blog />} />
+                  <Route path="item" element={<Item />} />
+                </Route>
               </Route>
-            </Route>
-          </Routes>
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </Wrapper>
         </BrowserRouter>
       </ThemeProvider>
     </div>
--- src/ui/pages/404.jsx
+++ src/ui/pages/404.jsx
@@ -1,7 +1,7 @@
 export default function Page404() {
   return (
-    <div>
-      <p>Hello Blog</p>
+    <div style={{ margin: '200px 100px',textAlign:'center' }}>
+      <h2>Page Not Found </h2>
     </div>
   );
 }
