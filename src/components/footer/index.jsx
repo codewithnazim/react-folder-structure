2d8f2331cd35d9048db4a4fb93493cfea6776ea4
smita-shinde203 <smitashinde203@gmail.com>
2022-04-26 20:41:30 +0530

    fixes added

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -12,17 +12,19 @@ const useStyles = makeStyles((theme) => ({
 export const Footer = () => {
   const classes = useStyles();
   return (
-    <Box sx={{ bgcolor: '#1E5DBB14', display: 'flex', alignItems: 'center', padding: '100px 10px', justifyContent:'center' }} >
+    <Box sx={{ bgcolor: '#1E5DBB14', display: 'flex', alignItems: 'center', padding: '100px 10px', justifyContent: 'center' }} >
       <Container fixed>
-        <Grid container marginX='auto' sx={{ display: 'flex', alignItems: 'flex-start', justifyContent:'center'}}>
+        <Grid container marginX='auto' sx={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'center' }}>
           <Grid item xs={12} sm={12} md={3} lg={3} >
-            <img src={require("../../ui/assets/footer/Mask group (2).png").default} alt="" />
+            <Box>
+              <img src={require("../../ui/assets/footer/Mask group (2).png").default} alt="" />
+            </Box>
           </Grid>
-          <Grid item xs={6} sm={4} md={2} lg={3} >
+          <Grid item xs={6} sm={4} md={3} lg={2}>
             <Typography variant='h5'>
               Links
             </Typography>
-            <Stack marginTop={2}  sx={{gap:'10px'}} >
+            <Stack marginTop={2} sx={{ gap: '10px' }}>
               {data1.map((item, index) =>
                 <Typography key={index} variant='body1' color='#00000'>
                   {item.title}
@@ -30,11 +32,11 @@ export const Footer = () => {
               )}
             </Stack>
           </Grid>
-          <Grid item xs={6} sm={4} md={3}  lg={3}>
+          <Grid item xs={6} sm={4} md={3} lg={3}>
             <Typography variant='h5'>
               Services
             </Typography>
-            <Stack marginTop={2}  sx={{gap:'10px'}}>
+            <Stack marginTop={2} sx={{ gap: '10px' }}>
               {data2.map((item, index) =>
                 <Typography key={index} variant='body1' color='#00000'>
                   {item.title}
@@ -47,31 +49,31 @@ export const Footer = () => {
               Connect
             </Typography>
             <Stack marginTop={2} >
-              <Box sx={{display:'flex', flexDirection:'column', gap:'10px' }}>
-              <Typography variant='h6'>
-                Email - support@hospital.com
-              </Typography>
-              <Typography variant='h6'>
-                Phone - +91 (731) 253-41-24
-              </Typography>
-              <Box flexDirection='row'  sx={{ display: 'flex', gap:'10px' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
                 <Typography variant='h6'>
-                  Location -
+                  Email - support@hospital.com
                 </Typography>
-                <Box flexWrap='wrap' width='60%'>
-                  <Typography  variant='h6'  >
-                    335, Jawahar Marg, Madhav Mandir Complex, Indore
+                <Typography variant='h6'>
+                  Phone - +91 (731) 253-41-24
+                </Typography>
+                <Box flexDirection='row' sx={{ display: 'flex', gap: '10px' }}>
+                  <Typography variant='h6'>
+                    Location -
                   </Typography>
+                  <Box flexWrap='wrap' width='60%'>
+                    <Typography variant='h6'  >
+                      335, Jawahar Marg, Madhav Mandir Complex, Indore
+                    </Typography>
+                  </Box>
                 </Box>
-              </Box> 
               </Box>
             </Stack>
           </Grid>
         </Grid>
         <Typography variant='body1' mt='20px'>
-        Copyright 2022. hospital all right reserved.
+          Copyright 2022. hospital all right reserved.
         </Typography>
-        </Container>
+      </Container>
     </Box>
   )
 }
